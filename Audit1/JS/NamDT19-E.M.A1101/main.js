fed3afa7339aedc92961b4526d6db736ebbc8869
Daothanhnam <88824678+daothanhnam@users.noreply.github.com>
2023-05-14 01:02:51 +0700

    :v:

---
+++ Audit1/JS/NamDT19-E.M.A1101/main.js
@@ -0,0 +1,41 @@
+const images = ["./images/img1.jpg", "./images/img2.png", "./images/img3.png"];
+let index = 0;
+ 
+// Add your code here
+// Task 1
+const imageContainer = document.querySelector(".img-container");
+const backBtn = document.querySelector(".back");
+const nextBtn = document.querySelector(".next");
+
+
+backBtn.addEventListener('click', function() {
+  // Add your code here
+  // Task 3.1
+  backBtn.addEventListener("click", function () {
+    index--;
+    if (index < 0) {
+      index = 0;
+    }
+    renderImage();
+  });
+});
+
+nextBtn.addEventListener('click', function() {
+  // Add your code here
+  // Task 3.2
+  nextBtn.addEventListener("click", function () {
+    index++;
+    if (index >= images.length) {
+      index = images.length - 1;
+    }
+    renderImage();
+  });
+});
+
+// Add your code here
+// Task 2
+const renderImage = () => {
+  imageContainer.innerHTML = `<img src="${images[index]}" alt="Image ${index}">`;
+};
+
+renderImage();
