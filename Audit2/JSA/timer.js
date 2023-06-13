36aa0e6a9a2bce4d8a405becd1c1175756a6aa96
nam <daothanhnam0923@gmail.com>
2023-06-13 19:15:57 +0700

    timer

---
+++ Audit2/JSA/timer.js
@@ -0,0 +1,32 @@
+// Timeout
+
+// let timeOutID_1 = setTimeout(() => {
+//   console.log("timeOutID_1");
+// }, 5000);
+
+// let timeOutID_2 = setTimeout(() => {
+//   console.log("timeOutID_2");
+//   clearTimeout(timeOutID_1);
+// }, 2000);
+
+// Interval
+
+// setInterval(() => {
+//   console.log("Interval");
+// }, 1000);
+
+// let count = 0;
+// let Interval_01 = setInterval(() => {
+//   console.log(count);
+//   count++;
+//   if (count > 10) {
+//     clearInterval(Interval_01);
+//   }
+// }, 200);
+
+console.log("AAA");
+
+setTimeout(() => {
+  console.log("BBB");
+}, 1000 );
+console.log("CCC");
